92f593b7343750a15d17d1ab1c115a0020eeddb0
Raquel007 <100597410+Raquel007@users.noreply.github.com>
2022-09-11 19:20:50 -0300

    creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { ExperienciaYEducacionComponent } from './components/experiencia-y-educacion/experiencia-y-educacion.component';
+import { LogrosComponent } from './components/logros/logros.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    AptitudesComponent,
+    ExperienciaYEducacionComponent,
+    LogrosComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
